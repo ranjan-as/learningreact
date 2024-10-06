6b34aa65a01a32bb9bf3b8d2532302b2312106ac
ranjan as <ranjanashish654@gmail.com>
2024-10-06 15:52:52 +0530

    test commit

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'; // Import the CSS file
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Developed by <a href="https://www.linkedin.com/in/ashish-ranjan-singh">Ashish Ranjan Singh</a></p>
+    </div>
+  );
+}
+
+export default Footer;
